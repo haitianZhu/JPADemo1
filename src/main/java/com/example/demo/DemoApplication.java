647cd351bf8514a38a7284b8189dd91341dcab51
haitianZhu <nova_zhuht@126.com>
2018-07-02 23:17:47 +0800

    添加邮件测试服务

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import com.example.demo.service.MailSenderService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -24,8 +25,18 @@ public class DemoApplication {
     public void test() {
         System.out.println("testMail");
 //        mailSenderService.sendSimpleMail("nova_zhuht@126.com", "测试标题", "测试内容" + new Date().toString());
-        mailSenderService.sendHTMLMail("nova_zhuht@126.com", "测试标题:HTML邮件");
 
+//        StringBuffer sb = new StringBuffer();
+//        sb.append("<h1>大标题-h1</h1>")
+//                .append("<p style='color:#F00'>红色字</p>")
+//                .append("<p style='text-align:right'>右对齐</p>");
+//        mailSenderService.sendHTMLMail("nova_zhuht@126.com", "测试标题:HTML邮件", sb.toString());
 
+//        mailSenderService.sendAttachmentsMail("nova_zhuht@126.com", "测试标题：带附件", "测试内容" + new Date()
+//                .toString());
+
+//        mailSenderService.sendInlineMail("nova_zhuht@126.com", "测试标题：带静态资源的邮件");
+
+        mailSenderService.sendTemplateMail("nova_zhuht@126.com", "测试标题:模板邮件");
     }
 }
--- src/main/java/com/example/demo/service/MailSenderService.java
+++ src/main/java/com/example/demo/service/MailSenderService.java
@@ -2,11 +2,14 @@ package com.example.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMailMessage;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -22,8 +25,11 @@ public class MailSenderService {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    @Autowired
+    private SpringTemplateEngine templateEngine;
+
     @Value("${spring.mail.username}")
-    private String fromName;
+    private String fromAddress;
 
     /**
      * 发送简单文本邮件
@@ -34,7 +40,7 @@ public class MailSenderService {
     public void sendSimpleMail(String toAddress, String title, String content) {
 
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(fromName);
+        message.setFrom(fromAddress);
         message.setTo(toAddress);
         message.setSubject(title);
         message.setText(content);
@@ -46,7 +52,7 @@ public class MailSenderService {
      * @param toAddress
      * @param title
      */
-    public void sendHTMLMail(String toAddress, String title) {
+    public void sendHTMLMail(String toAddress, String title, String htmlContent) {
 
         MimeMessage message = null;
 
@@ -54,18 +60,89 @@ public class MailSenderService {
 
             message = javaMailSender.createMimeMessage();
             MimeMessageHelper messageHelper = new MimeMessageHelper(message, true);
-            messageHelper.setFrom(fromName);
+            messageHelper.setFrom(fromAddress);
             messageHelper.setTo(toAddress);
             messageHelper.setSubject(title);
-            StringBuffer sb = new StringBuffer();
-            sb.append("<h1>大标题-h1</h1>")
-                    .append("<p style='color:#F00'>红色字</p>")
-                    .append("<p style='text-align:right'>右对齐</p>");
-            messageHelper.setText(sb.toString(), true);
+
+            messageHelper.setText(htmlContent, true);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
         javaMailSender.send(message);
+    }
+
+    /**
+     * 发送带附件到邮件
+     * @param toAddress
+     * @param title
+     * @param content
+     */
+    public void sendAttachmentsMail(String toAddress, String title, String content) {
+
+        MimeMessage message = null;
+
+        try {
+            message = javaMailSender.createMimeMessage();
+            MimeMessageHelper messageHelper = new MimeMessageHelper(message, true);
+            messageHelper.setFrom(fromAddress);
+            messageHelper.setTo(toAddress);
+            messageHelper.setSubject(title);
+            messageHelper.setText(content);
+
+            FileSystemResource file = new FileSystemResource("src/main/resources/static/favicon.ico");
+
+            messageHelper.addAttachment("图片.jpg", file);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+        javaMailSender.send(message);
+    }
+
+    /**
+     * 发送带静态资源的邮件
+     * @param toAddress
+     * @param title
+     */
+    public void sendInlineMail(String toAddress, String title) {
+
+        MimeMessage message = null;
+
+        try {
+            message = javaMailSender.createMimeMessage();
+            MimeMessageHelper messageHelper = new MimeMessageHelper(message, true);
+            messageHelper.setFrom(fromAddress);
+            messageHelper.setTo(toAddress);
+            messageHelper.setSubject(title);
+            // 第二个参数指定发送的是HTML格式，同时cid:是固定的写法
+            messageHelper.setText("<html><body>带静态资源的邮件内容 图片:<img src='cid:picture' " +
+                    "/></body></html>", true);
+
+
+            FileSystemResource fileSystemResource = new FileSystemResource
+                    ("src/main/resources/static/picture.jpg");
+
+            messageHelper.addInline("picture", fileSystemResource);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+        javaMailSender.send(message);
+    }
+
+    /**
+     * 发送模板邮件
+     * @param toAddress
+     * @param title
+     */
+    public void sendTemplateMail(String toAddress, String title) {
+
+        //创建邮件正文
+        Context context = new Context();
+        context.setVariable("id", "006");
+        String emailContent = templateEngine.process("emailTemplate", context);
+
+        sendHTMLMail(toAddress, title, emailContent);
     }
 }
